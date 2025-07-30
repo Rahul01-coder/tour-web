e3d2ac2406b2dbbda202c1ad6e10351df97291ad
Rahul <rparjapati123456@gmail.com>
2025-07-29 23:56:33 -0700

    main react logic is complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,36 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Tour from './component/Tour'
+import Data from './Data'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [tours, setTours] = useState(Data);
+
+  function removeTour(id){
+    const newTour = tours.filter(tour => tour.id !== id);
+    setTours(newTour);
+  }
+
+  function refreshbtn(){
+    return setTours(Data);
+  }
+
+  if(tours.length === 0){
+    return(
+      <div className='refresh'>
+        <h2>No Tours Left</h2>
+        <button className='btn-white' onClick={refreshbtn}>
+          refresh
+        </button>
+      </div>
+    )
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className='App'>
+      <Tour tours = {tours} removeTour = {removeTour}/> 
+    </div>
   )
 }
 
---
+++ src/component/Tour.jsx
@@ -0,0 +1,19 @@
+import Card from "./Card"
+
+export default function Tour({tours,removeTour}){
+    return (
+        <div className="container">
+            <div>
+                <h2 className="title">Plan with Love</h2>
+            </div>
+
+            <div className="cards">
+                {
+                    tours.map((tour) =>{
+                        return <Card {...tour} key={tour.id} removeTour = {removeTour}/>
+                    })
+                }
+            </div>
+        </div>
+    )
+}
